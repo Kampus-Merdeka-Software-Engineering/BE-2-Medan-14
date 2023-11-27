5bc22e8a82be86cb54ff3a253084b37d3a6ae27e
Rai Pramana <rai.pramana46@gmail.com>
2023-11-27 14:25:50 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(
         store: store,
         cookie: {
             sameSite: "none", // must be 'none' to enable cross-site delivery
-            secure: false, // must be true if sameSite='none'
+            secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
             httpOnly: true,
         },
     })
